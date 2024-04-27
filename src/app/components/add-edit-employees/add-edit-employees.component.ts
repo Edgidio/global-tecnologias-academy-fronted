4036226de1134f889c60a418ae0d1cac0e4b88e8
Edgidio Leon <edgidio.leon@gmail.com>
2024-04-27 10:53:10 -0400

    Empezando el frontend

---
+++ src/app/components/add-edit-employees/add-edit-employees.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-employees',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-edit-employees.component.html',
+  styleUrl: './add-edit-employees.component.css'
+})
+export class AddEditEmployeesComponent {
+
+}
---
+++ src/app/interfaces/employee.interface.ts
@@ -0,0 +1,16 @@
+export interface employee {
+    id?:  number,  
+    firstName:  string,  
+    lastName:  string, 
+    otherNames?: string 
+    country:  string,  
+    identificationType: string,  
+    identificationNumber?:  number,  
+    email:  string,  
+    dateOfEntry:  string,  
+    area:  string,  
+    status?:  string,  
+    createdAt?:  string,  
+    updatedAt?:  string  
+
+}
